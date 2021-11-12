4a68ac3c26958b6cf4216f9ad54a377866299458
Nitesh <singhnitesh3712@gmail.com>
2021-11-12 16:06:55 +0530

    added helper methods to encrypt messages

---
+++ emitter/lib/helper.js
@@ -0,0 +1,37 @@
+const crypto = require('crypto');
+const data = require('../../static-files/data.json');
+
+const encryptMessage = () => {
+    let originalMessage = {
+        name: data.names[Math.floor(Math.random() * data.names.length)],
+        origin: data.cities[Math.floor(Math.random() * data.cities.length)],
+        destination: data.cities[Math.floor(Math.random() * data.cities.length)]
+    }
+
+    let originalMessageWithHash = Object.assign({}, originalMessage, crypto.createHash('sha256').update(JSON.stringify(originalMessage)));
+
+    const algorithm = 'aes-256-ctr';
+    const secretKey = 'smartjouleskey';
+
+    let cipher = crypto.createCipheriv(algorithm, secretKey);
+
+    let encryptedMessage = Buffer.concat([cipher.update(JSON.stringify(originalMessageWithHash)), cipher.final()]);
+
+    return encryptedMessage;
+}
+
+const generateEncryptedPayload = () => {
+    let min = 49;
+    let max = 499;
+    let random = Math.floor((Math.random() * (max - min) + min));
+    let encryptedPayload = "";
+    for(let i = 0; i< random; i++) {
+        encryptedPayload += encryptMessage() + '|';
+    }
+    encryptedPayload = encryptedPayload.substring(0, encryptedPayload.length - 1); // to remove the last pipe ('|') symbol
+    return encryptedPayload;
+}
+
+module.exports = {
+    generateEncryptedPayload
+}
